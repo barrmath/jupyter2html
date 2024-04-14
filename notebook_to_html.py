2e2d5c3d00eee09c62ff350e2e9add220a3a1221
tux <mathieu.barrios@outlook.com>
2024-04-14 10:51:16 +0200

    ajout de la création de l índex + mise en place du changement d'extension (ipynb passe a html)

--- notebook_to_html.py
+++ notebook_to_html.py
@@ -5,21 +5,45 @@ from os import mkdir
 # lister les notebooks present à la racine
 all_file = listdir()
 
+# liste vide, servira pour crée un index avec les adresses des notebooks
+notebook = []
+
 # créer le dossier html et gerer si le dossier existe deja
 try:
-    mkdir('html')
+    mkdir("html")
 except OSError as error:
     # print(error)
     pass
 
 # pour chaque fichier notebook faire la convertion et envoyer la copie html vers le dossier html
 for file in all_file:
-    if file.endswith('.ipynb'):
+    if file.endswith(".ipynb"):
         # faire la convertion avec un object
         htmlexport = HTMLExporter()
         htmlexport.exclude_input = True
-        htmldata,ressource = htmlexport.from_file(file)
+        htmldata, ressource = htmlexport.from_file(file)
+        notebook.append(file[:-6] + ".html")
         # write to output file
-        outputdata = 'html/'+file
+        outputdata = "html/" + file[:-6] + ".html"
         with open(outputdata, "w") as f:
             f.write(htmldata)
+
+# faire un fichier index.html avec les liens vers les page html notebook
+
+html_text = """<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>index</title>
+  </head>
+  <body>
+  <p>index généré par <a href=https://github.com/barrmath/jupyter2html >https://github.com/barrmath/jupyter2html</a><br><br>\n"""
+
+
+for file in notebook:
+    html_text += "<a href= " + file + ' target="blank">' + file[:-5] + "</a>\n<br>"
+
+html_text += "</p></body>"
+
+with open("html/index.html", "w") as f:
+    f.write(html_text)
